ef006b173d7a1cda2720884d95117d20f47726cb
krishna <krishnakushwaha743@gmail.com>
2024-03-13 18:11:32 +0530

    added transparent header

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <AppLayout>
         <TodoForm />
       </AppLayout>
+      
     </div>
   );
 }
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import { Layout } from "antd";
 import Header from "../header/Header";
+import HeaderTwo from "../header2/Header2";
 
 const { Content } = Layout;
 
@@ -11,7 +12,8 @@ interface AppLayoutProps {
 const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   return (
     <Layout className="bg-background">
-      <Header />
+      {/* <Header /> */}
+      <HeaderTwo/>
       <Content className=" text-white p-6">{children}</Content>
     </Layout>
   );

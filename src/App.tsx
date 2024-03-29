0f38f4559f12d4f81bbed35014041de2116372e1
krishna <krishnakushwaha743@gmail.com>
2024-03-29 15:33:58 +0530

    bugfixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-import AppLayout from "./components/layout/Layout";
-import TodoForm from "./pages/TodoForm";
 
 
 function App() {
   return (
     <div className="App">
-      <AppLayout>
-        {/* <TodoForm /> */}
-      </AppLayout>
+      
       
     </div>
   );
